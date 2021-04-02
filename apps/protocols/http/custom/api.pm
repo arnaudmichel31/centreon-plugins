be4473f61dc80e44ec4328eddb8f7a0047804c99
root <arnmichel@gmail.com>
2021-04-02 14:46:50 +0100

    api_key V1

--- apps/protocols/http/custom/api.pm
+++ apps/protocols/http/custom/api.pm
@@ -48,6 +48,9 @@ sub new {
             'api-proto:s'        => { name => 'api_proto' },
             'api-username:s' => { name => 'api_username' },
             'api-password:s' => { name => 'api_password' },
+            'api-key:s' => { name => 'api_key' },
+            'api-grant_type:s' => { name => 'api_grant_type' , default => 'api_key' },
+            'api-client_id:s' => { name => 'api_client_id' , default => 'GDS' },
             'api-path:s'     => { name => 'api_path' },
             'timeout:s'      => { name => 'timeout' },
             'api-tokenexpires:s'      => { name => 'api_tokenexpires' , default => 3600 }
@@ -86,6 +89,12 @@ sub check_options {
            $self->{option_results}->{api_username} : undef;
        $self->{api_password} = (defined($self->{option_results}->{api_password})) ?
            $self->{option_results}->{api_password} : undef;
+       $self->{api_key} = (defined($self->{option_results}->{api_key})) ?
+           $self->{option_results}->{api_key} : undef;
+       $self->{api_grant_type} = (defined($self->{option_results}->{api_grant_type})) ?
+           $self->{option_results}->{api_grant_type} : undef;
+       $self->{api_client_id} = (defined($self->{option_results}->{api_client_id})) ?
+           $self->{option_results}->{api_client_id} : undef;
        $self->{api_path} = (defined($self->{option_results}->{api_path})) ?
            $self->{option_results}->{api_path} : '/centreon/api/index.php';
        $self->{timeout} = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 10;
@@ -95,14 +104,20 @@ sub check_options {
            $self->{output}->add_option_msg(short_msg => "Need to specify --hostname option.");
            $self->{output}->option_exit();
        }
-       if (!defined($self->{api_username}) || $self->{api_username} eq '') {
-           $self->{output}->add_option_msg(short_msg => "Need to specify --api-username option.");
-           $self->{output}->option_exit();
-       }
-       if (!defined($self->{api_password}) || $self->{api_password} eq '') {
-           $self->{output}->add_option_msg(short_msg => "Need to specify --api-password option.");
-           $self->{output}->option_exit();
-      }
+
+       #api_key check if username and password not set
+       if (!defined($self->{api_key}) || $self->{api_key} eq '') {
+           if (!defined($self->{api_username}) || $self->{api_username} eq '') {
+              $self->{output}->add_option_msg(short_msg => "Need to specify --api-username or --api-key option.");
+              $self->{output}->option_exit();
+           }
+
+           if (!defined($self->{api_password}) || $self->{api_password} eq '') {
+              $self->{output}->add_option_msg(short_msg => "Need to specify --api-password or --api-key option.");
+              $self->{output}->option_exit();
+           } 
+       } 
+          
       
        $self->{cache}->check_options(option_results => $self->{option_results});
    }
@@ -144,14 +159,28 @@ sub settings {
 
 sub get_auth_token {
     my ($self, %options) = @_;
+    my $has_cache_file;
+    my $post_param;
 
-    my $has_cache_file = $options{statefile}->read(statefile => $self->{option_results}->{authtoken} . '_' . md5_hex($self->{hostname}) .
-        '_' . md5_hex($self->{api_username}));
+    if ( !defined($self->{api_key}) ) {
+       $has_cache_file = $options{statefile}->read(statefile => $self->{option_results}->{authtoken} . '_' . md5_hex($self->{hostname}) .
+          '_' . md5_hex($self->{api_username}));
+    } else {
+       $has_cache_file = $options{statefile}->read(statefile => $self->{option_results}->{authtoken} . '_' . md5_hex($self->{hostname}) .
+          '_' . md5_hex($self->{api_key}));
+    }
+    
     my $expires_on = $options{statefile}->get(name => 'expires_on');
     my $auth_token = $options{statefile}->get(name => 'auth_token');
 
     if ($has_cache_file == 0 || !defined($auth_token) || (($expires_on - time()) < 10)) {
-        my $post_param = [ 'username=' . $self->{api_username}, 'password=' . $self->{api_password} ];
+        #post API key instead of username password if needed
+        if ( !defined($self->{api_key}) ) {  
+            $post_param = [ 'username=' . $self->{api_username}, 'password=' . $self->{api_password} ];
+        } else {
+            $post_param = [ 'apikey=' . $self->{api_key}, 'grant_type=' . $self->{api_grant_type}, 'client_id=' . $self->{api_client_id} ];
+        }
+
         $self->settings();
 
        
@@ -170,14 +199,21 @@ sub get_auth_token {
             $self->{output}->add_option_msg(short_msg => "Cannot decode json response");
             $self->{output}->option_exit();
         }
+        
+        $auth_token = $decoded->{authToken};
+
         if (!defined($decoded->{authToken})) {
-            $self->{output}->add_option_msg(short_msg => "Cannot get token");
-            $self->{output}->option_exit();
+           if (!defined($decoded->{access_token})) {
+              $self->{output}->add_option_msg(short_msg => "Cannot get token");
+              $self->{output}->option_exit();
+           }
+           $auth_token = 'Bearer ' . $decoded->{access_token};
         }
 
-        $auth_token = $decoded->{authToken};
+        #$auth_token = $decoded->{authToken};
         #remplacer 3600 par var api-tokenexpire
-        my $datas = { last_timestamp => time(), auth_token => $decoded->{authToken}, expires_on => time() + $self->{api_tokenexpires} };
+        # my $datas = { last_timestamp => time(), auth_token => $decoded->{authToken}, expires_on => time() + $self->{api_tokenexpires} };
+        my $datas = { last_timestamp => time(), auth_token => $auth_token , expires_on => time() + $self->{api_tokenexpires} };
         $options{statefile}->write(data => $datas);
     }
     
@@ -267,17 +303,17 @@ Centreon Rest API custom mode
 
 =over 8
 
-=item B<--hostname>
+=item B<--api-hostname>
 
-Centreon hostname.
+Centreon api-hostname.
 
-=item B<--port>
+=item B<--api-port>
 
-Port used (Default: 80)
+Port used (Default: 80).
 
-=item B<--proto>
+=item B<--api-proto>
 
-Specify https if needed (Default: 'http')
+Specify https if needed (Default: 'http').
 
 =item B<--api-username>
 
@@ -286,6 +322,18 @@ Centreon username.
 =item B<--api-password>
 
 Centreon password.
+
+=item B<--api-key>
+
+Centreon api-key if api-username and api-password undefined.
+
+=item B<--api-grant_type>
+
+Centreon api-grant_type if --api-key defined (Default : 'api_key').
+
+=item B<--api-client_id>
+
+Centreon api-client_id if --api-key defined (Default : 'GDS').
 
 =item B<--api-path>
 
